3f53070d70559d8409c6f2228b0ec6ba5697d0e6
viktorasj <33597800+viktorasj@users.noreply.github.com>
2017-12-02 01:50:33 +0200

    veikia playeris ir gallery

--- js/script.js
+++ js/script.js
@@ -1,29 +1,59 @@
-window.onload = function () {
-function doStuff() {
-document.getElementById('musicWindow').style.visibility = "visible"; //reikalingas ijungti music div'a;
-document.getElementById("svg-line-first").style.filter = "grayscale(0%)";
-$("main.col-md-7").css("border-left","#FE01E7 1px solid");
-$("main.col-md-7").css("border-right","#FE01E7 1px solid");
-for (var i = 0; i < mainWArray.length; i++) {
-    console.log(mainWArray);
-    if (mainWArray[i] == event.target.getAttribute('nrl')) {
-        console.log("atidaro ", i, "langa");
-        $('.mainWindows').eq(i).animate({opacity: 1}, 500).show();
-        mainW[i].style.backgroundColor = "rgba(0, 0, 0, 1)";
-        document.getElementById("svg-line-first").style.opacity = "0";
-    }
-    else if(mainWArray[i] != event.target.getAttribute('nrl')) {
-        mainW[i].style.backgroundColor = "rgba(0, 0, 0, 0)";
-        $('.mainWindows').eq(i).animate({opacity: 0}, 800).hide();
-    }
-  }
-}
+window.onload = function() {
 
-function refresh(){
-    for (var i = 0; i < mainWArray.length; i++){
-        mainW[i].style.display = "none";
+
+    function doStuff() {
+
+
+        $('#musicWindow').css("visibility", "visible"); //reikalingas ijungti music div'a;
+        document.getElementById("svg-line-first").style.filter = "grayscale(0%)";
+        $("main.mdl-cell--7-col").css("border-left", "#FE01E7 1px solid");
+        $("main.mdl-cell--7-col").css("border-right", "#FE01E7 1px solid");
+        for (var i = 0; i < mainWArray.length; i++) {
+            if (mainWArray[i] == event.target.getAttribute('nrl')) {
+                console.log("atidaro ", i, "langa");
+                $('.mainWindows').eq(i).animate({
+                    opacity: 1
+                }, 500).show();
+                mainW[i].style.backgroundColor = "rgba(0, 0, 0, 1)";
+                document.getElementById("svg-line-first").style.opacity = "0";
+            } else if (mainWArray[i] != event.target.getAttribute('nrl')) {
+                mainW[i].style.backgroundColor = "rgba(0, 0, 0, 0)";
+                $('.mainWindows').eq(i).animate({
+                    opacity: 0
+                }, 800).hide();
+            }
+        }
     }
-}
+
+    function refresh() {
+        for (var i = 0; i < mainWArray.length; i++) {
+            mainW[i].style.display = "none";
+        }
+    }
+
+    function showPlayer (e) {
+        console.log(playerDivs);
+        for (var i = 0; i < playerDivs.length; i++) {
+
+        if (e.target.attributes.linkid.value === playerDivs[i].attributes[2].value){
+                $("#divForIframe").slideDown("slow");
+                $(".visIframes").eq(i).fadeIn(3000);
+                var currentSrc = playerDivs[i].getAttribute('data-src');
+                playerDivs[i].setAttribute('src', '');
+                playerDivs[i].setAttribute('src', currentSrc);
+                }
+        }
+        $('#music-cross-button').click(function(){
+
+            $(".visIframes").fadeOut(3000, function (){
+                $('#divForIframe').slideUp("slow");
+                console.log(playerDivs);
+                console.log(document.getElementById('#divForIframe'));
+        });
+    });
+
+    }
+
 
 
 
@@ -34,9 +64,11 @@ function refresh(){
     var debesis = document.getElementById('deb');
     var clickable = document.getElementById('clickable');
     var expGallery = document.getElementById('exp-gallery');
-    console.log(sideLinks);
-    console.log(mainW);
     var mainWArray = [];
+    var albumLinks = document.getElementsByClassName('art');
+    var playerDivs = document.getElementsByClassName('visIframes');
+
+
 
 
 
@@ -47,51 +79,43 @@ function refresh(){
     for (i = 0; i < mainW.length; i++) {
         mainWArray.push(mainW[i].getAttribute("nrw"));
     }
-    console.log(mainWArray);
+
+
+    for (i = 0; i < playerDivs.length; i++) {
+        playerDivs[i].setAttribute("playerid", "music"+[i]);
+    }
+
+    for (i = 0; i < albumLinks.length; i++) {
+        albumLinks[i].setAttribute("linkid", "music"+[i]);
+        albumLinks[i].addEventListener('click', showPlayer);
+    }
 
     swNav.addEventListener('click', refresh);
 
-    debesis.onmouseup = function (){
-        lietus.style.opacity = 0;
+    debesis.onmouseup = function() {
+        lietus.style.display = "none";
+    };
+
+    debesis.onmousedown = function() {
+        lietus.style.display = "block";
+    };
+
+    debesis.ondblclick = function() {
+        lietus.style.display = "block";
+    };
+
+
+    $(".sideLinks").click(function() {
+        $(".sideLinks").removeClass("active");
+        $(this).addClass("active");
+    });
+
+
+
+    $("#draggable").draggable();
+
+
+    $("#rain").draggable();
+
+
 };
-
-debesis.onmousedown = function (){
-    lietus.style.opacity = 0.5;
-};
-
-debesis.ondblclick = function (){
-    lietus.style.opacity = 0.5;
-};
-
-
-$(".sideLinks").click(function(){
-  $(".sideLinks").removeClass("active");
-  $(this).addClass("active");
-});
-
-
-
-$( "#draggable" ).draggable();
-
-$( "#rain" ).draggable();
-};
-
-
-
-$(document).ready(function(){
-    $(window).scroll(function(){
-       $( "list-group-item").next().addClass( "active" );
-       $( "list-group-item").removeClass("active");
-   });
-});
-
-// slider for
-
-$(document).ready(function(){
-   $('.slider').bxSlider({
-       responsive: true,
-       pager: false,
-       randomStart: true,
-       keyboardEnabled: true
-   });
- });
--- testavimui/js/script.js
+++ testavimui/js/script.js
@@ -1,8 +1,28 @@
- $(document).ready(function(){
-    $('.slider').bxSlider({
-        auto: true,
-        autoControls: true,
-        pager: true,
-        slideWidth: 600
-    });
-  });
+var albumLinks = document.getElementsByClassName('art');
+var playerDivs = document.getElementsByClassName('visIframes');
+var divForIframe = document.getElementById('divForIframe');
+console.log(albumLinks);
+
+for (var i = 0; i < playerDivs.length; i++) {
+    playerDivs[i].setAttribute("playerid", "music"+[i]);
+}
+for (var i = 0; i < albumLinks.length; i++) {
+    albumLinks[i].setAttribute("linkid", "music"+[i]);
+    albumLinks[i].addEventListener('click', showPlayer);
+}
+
+function showPlayer (e) {
+    console.log(playerDivs);
+    for (var i = 0; i < playerDivs.length; i++) {
+
+    if (e.target.attributes.linkid.value === playerDivs[i].attributes[2].value){
+            divForIframe.style.display = "block";
+            playerDivs[i].style.display = "block";
+
+            var currentSrc = playerDivs[i].getAttribute('data-src');
+            playerDivs[i].setAttribute('src', '');
+            playerDivs[i].setAttribute('src', currentSrc);
+            }
+    }
+
+}
