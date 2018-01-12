0f3aee095a5cd6e369f7728b2211fde2d8f126b4
DESKTOP-MKB5IS5\Vic <viktoras.jefimovas@gmail.com>
2018-01-13 01:58:22 +0200

    + PC uzduotis

---
+++ PC uzduotis/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Alga</title>
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <script type="text/javascript" src="./lib/jQuery/jquery-3.2.1.min.js" defer> </script>
+    <script type="text/javascript" src="./js/myscript.js" defer> </script>
+
+
+  </head>
+  <body>
+
+  </body>
+</html>
