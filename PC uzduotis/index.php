708bdc543eb2058ec31c5f132fa1249f94677e95
DESKTOP-MKB5IS5\Vic <viktoras.jefimovas@gmail.com>
2018-01-13 14:38:23 +0200

    +js+style

--- PC uzduotis/index.php
+++ PC uzduotis/index.php
@@ -5,8 +5,6 @@
     <title>Atlyginimo skaičiuoklė</title>
     <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/style.css">
-    <script type="text/javascript" src="./lib/jQuery/jquery-3.2.1.min.js" defer> </script>
-    <script type="text/javascript" src="./js/myscript.js" defer> </script>
 
 
   </head>
@@ -29,27 +27,53 @@
     <div class="row au">
       <div class="col-md-12 d-flex align-items-center justify-content-center">
         <span class="atl-span mr-2 mb-1 d-flex align-items-center justify-content-center">Skaičiuosime atlyginimą</span>
-        <select class="atl-choose" name="">
+        <select class="atl-choose" name="" id="selectedInput">
           <option value="i_rankas">Į rankas</option>
           <option value="ant_popieriaus">Ant popieriaus</option>
         </select>
-        <button class="btn btn-primary ml-3" type="submit" name="selection">Pasirinkti</button>
       </div>
     </div>
 
     <!-- Atlyginimo ivedimas -->
 
-    <div class="row au">
+    <div class="row">
       <div class="bg-info col-md-12">
-        <h3>Atlyginimo ivedimas</h3>
+
+        <!-- Atlyginimo ant popieriaus ivedimas -->
+
+        <div id="atl_antpopier_input" class="mt-3 mb-2 text-center dn">
+          <input id="atl-input" type="number" name="atl_antpopier_input" value="" placeholder="Įveskite atlyginimą ant popieriaus">
+        </div>
+
+        <!-- Atlyginimo i rankas ivedimas -->
+
+        <div id="atl_irankas_input" class="mt-3 mb-2 text-center dn">
+          <input id="atl-input" type="number" name="atl_irankas_input" value="" placeholder="Įveskite atlyginimą į rankas">
+        </div>
+
       </div>
     </div>
-
+    <hr>
     <!-- Duomenys -->
+    <div class="row">
+      <div class="col-md-12">
 
-    <div class="row aux">
-      <div class="bg-success col-md-12">
-      <h3>Duomenys</h3>
+      <div id="atl_irankas_skaiciuokle" class="dn">
+        <h4>Priskaičiuotas atlyginimas "ant popieriaus" - <span id="testSpan"></span></h4>
+        <h4>Sodros mokesčiai:</h4>
+        <h5 class="ml-4">Pajamų mokestis 15%</h5>
+        <h5 class="ml-4">Sodra. Sveikatos draudimas 6%</h5>
+        <h5 class="ml-4">Sodra. Pensijų ir soc. draudimas 3%</h5>
+        <h4>Išmokamas atlyginimas "į rankas"</h4>
+        <br>
+        <h4>Darbdavio sumokami mokesčiai - Sodra - 31.18%</h4>
+        <h4>Visa darbo vietos kaina</h4>
+      </div>
+
+      <div id="atl_antPopier_skaiciuokle" class="dn">
+        <h3>Atlyginimo ant popieriaus skaiciuokle</h3>
+      </div>
+
     </div>
   </div>
 
@@ -65,5 +89,7 @@
 
     </div>
 </div>
+<script type="text/javascript" src="./lib/jQuery/jquery-3.2.1.min.js" defer> </script>
+<script type="text/javascript" src="./js/myscript.js" defer> </script>
 </body>
 </html>
--- PC uzduotis/js/myscript.js
+++ PC uzduotis/js/myscript.js
@@ -1,1 +1,49 @@
+var selectedInput = document.getElementById("selectedInput");
+var selectedAtlValue = selectedInput.options[selectedInput.selectedIndex].value;
 
+var atl_antpopier_input = document.getElementById('atl_antpopier_input');
+var atl_irankas_input = document.getElementById('atl_irankas_input');
+
+var atl_irankas_skaiciuokle = document.getElementById('atl_irankas_skaiciuokle');
+var atl_antPopier_skaiciuokle = document.getElementById('atl_antPopier_skaiciuokle');
+var testSpan = document.getElementById('testSpan');
+
+
+function inputAtlIRankas () {
+  atl_antpopier_input.classList.remove("dn");
+  atl_irankas_input.classList.add("dn");
+  atl_irankas_skaiciuokle.classList.remove("dn");
+  atl_antPopier_skaiciuokle.classList.add('dn');
+  $('#atl-input').on('input', function (){
+    testSpan.innerHTML = $('#atl-input').val();
+
+  });
+
+
+
+}
+
+function inputAtlAntPopier () {
+  atl_antpopier_input.classList.add("dn");
+  atl_irankas_input.classList.remove('dn');
+  atl_irankas_skaiciuokle.classList.add("dn");
+  atl_antPopier_skaiciuokle.classList.remove('dn');
+
+
+}
+
+
+
+inputAtlIRankas ();
+
+
+
+$('#selectedInput').on('change', function (){
+  var selectedAtlValue = selectedInput.options[selectedInput.selectedIndex].value;
+  if (selectedAtlValue === "i_rankas") {
+    inputAtlIRankas ();
+  }
+  else if (selectedAtlValue === "ant_popieriaus") {
+    inputAtlAntPopier ();
+  }
+});
