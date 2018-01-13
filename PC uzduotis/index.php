51b5c94987fa587c142906f4ab1fae9d2aaf382e
DESKTOP-MKB5IS5\Vic <viktoras.jefimovas@gmail.com>
2018-01-13 03:19:15 +0200

    +style

--- PC uzduotis/index.php
+++ PC uzduotis/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Alga</title>
+    <title>Atlyginimo skaičiuoklė</title>
     <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <script type="text/javascript" src="./lib/jQuery/jquery-3.2.1.min.js" defer> </script>
@@ -13,34 +13,48 @@
   <body>
   <div class="container">
   <!-- Vietoj Footer ROW -->
+
+    <div class="row">
+        <div class="col-md-10 d-flex align-items-center justify-content-center">
+          <h3>Atlyginimo skaičiuoklė</h3>
+        </div>
+        <div class="col-md">
+          <button class="cfg-button" type="button" name="button">CFG</button>
+        </div>
+    </div>
+    <hr>
+
+  <!-- Atlyginimo pasirinkimas -->
+
     <div class="row au">
-        <div class="bg-info col-md-7">
-          <h3>Algų skaičiuoklė</h3>
-        </div>
-        <div class="bg-success col-md-3">
-          CFG button
-        </div>
+      <div class="col-md-12 d-flex align-items-center justify-content-center">
+        <span class="atl-span mr-2 mb-1 d-flex align-items-center justify-content-center">Skaičiuosime atlyginimą</span>
+        <select class="atl-choose" name="">
+          <option value="i_rankas">Į rankas</option>
+          <option value="ant_popieriaus">Ant popieriaus</option>
+        </select>
+        <button class="btn btn-primary ml-3" type="submit" name="selection">Pasirinkti</button>
+      </div>
     </div>
 
-  <!-- Algos pasirinkimas -->
-    <div class="row au">
-      <div class="bg-warning col-md-12">
-        <h3>Kokia alga? i rankas ar ant popieriaus</h3>
-      </div>
-    </div>
-    <!-- Algos ivedimas -->
+    <!-- Atlyginimo ivedimas -->
+
     <div class="row au">
       <div class="bg-info col-md-12">
-        <h3>Algos ivedimas</h3>
+        <h3>Atlyginimo ivedimas</h3>
       </div>
     </div>
+
     <!-- Duomenys -->
+
     <div class="row aux">
       <div class="bg-success col-md-12">
       <h3>Duomenys</h3>
     </div>
   </div>
+
       <!-- Autorines sutartys -->
+
     <div class="row au">
        <div class="bg-warning col-md-6"></div> <!-- nenaudojamas plotas -->
        <div class="bg-info col-md-6"> <!-- autorines sutartys -->
