a984aaf454d8cda30b548f03e94ad0cb19e15a98
vik <viktoras.jefimovas@gmail.com>
2018-08-15 17:52:54 +0300

    no message

--- #4_Serrano_demo/php/includes/admin_footer.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript" src="../libs/jQuery/jquery-3.2.1.min.js" defer></script>
-<script type="text/javascript" src="../libs/bootstrap/js/bootstrap.min.js" defer></script>
-<script type="text/javascript" src="../js/admin_scripts/add_food.js" defer></script>
-<script type="text/javascript" src="../js/admin_scripts/edit_food.js" defer></script>
-</body>
-
-</html>
