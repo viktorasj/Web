52847853f2f7a1b04dfd274012bccbb8e12c52da
vik <viktoras.jefimovas@gmail.com>
2018-09-12 22:47:29 +0300

    no message

---
+++ NFQ/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+      <link rel="stylesheet" href="./libs/bootstrap/css/bootstrap.min.css" />
+  </head>
+  <body>
+
+  </body>
+</html>
